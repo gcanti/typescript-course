4db25a4549e68c46993bf4d7e4e493bed5b3e2f8
gcanti <giulio.canti@gmail.com>
2018-06-03 19:31:43 +0200

    small fixes

--- solutions/04/4.0.1.ts
+++ solutions/04/4.0.1.ts
@@ -4,18 +4,28 @@ export declare function sequence<T>(
 
 // step 1
 
-const TODO: any = null
+declare const TODO: any
 
-function sequence1<T>(promises: Array<Promise<T>>): Promise<Array<T>> {
-  const f: (acc: Promise<Array<T>>, x: Promise<T>) => Promise<Array<T>> = TODO
+function sequence1<T>(
+  promises: Array<Promise<T>>
+): Promise<Array<T>> {
+  const f: (
+    acc: Promise<Array<T>>,
+    x: Promise<T>
+  ) => Promise<Array<T>> = TODO
   const init: Promise<Array<T>> = TODO
   return promises.reduce(f, init)
 }
 
 // step 2
 
-function sequence2<T>(promises: Array<Promise<T>>): Promise<Array<T>> {
-  const f: (acc: Promise<Array<T>>, x: Promise<T>) => Promise<Array<T>> = TODO
+function sequence2<T>(
+  promises: Array<Promise<T>>
+): Promise<Array<T>> {
+  const f: (
+    acc: Promise<Array<T>>,
+    x: Promise<T>
+  ) => Promise<Array<T>> = TODO
   const init: Promise<Array<T>> = Promise.resolve([])
   return promises.reduce(f, init)
 }
@@ -28,7 +38,9 @@ function sequence2<T>(promises: Array<Promise<T>>): Promise<Array<T>> {
 //   f: (a: A, b: B) => C
 // ): (fa: Promise<A>, fb: Promise<B>) => Promise<C>
 
-function sequence3<T>(promises: Array<Promise<T>>): Promise<Array<T>> {
+function sequence3<T>(
+  promises: Array<Promise<T>>
+): Promise<Array<T>> {
   const f = liftA2<Array<T>, T, Array<T>>(push)
   const init: Promise<Array<T>> = Promise.resolve([])
   return promises.reduce(f, init)
@@ -46,12 +58,12 @@ function liftA2<A, B, C>(
   return (fa, fb) => fa.then(a => fb.then(b => f(a, b)))
 }
 
-function sequence4<T>(promises: Array<Promise<T>>): Promise<Array<T>> {
-  const f = liftA2<Array<T>, T, Array<T>>(push)
-  const init: Promise<Array<T>> = Promise.resolve([])
-  return promises.reduce(f, init)
-}
+const promises = [
+  Promise.resolve(1),
+  Promise.resolve(2),
+  Promise.resolve(3)
+]
 
-const promises = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)]
+sequence3(promises).then(x => console.log(x)) // [1, 2, 3]
 
-sequence4(promises).then(x => console.log(x)) // [1, 2, 3]
+// ts-node solutions/04/4.0.1.ts
